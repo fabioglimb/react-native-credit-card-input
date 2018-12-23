27ece42382aea4b91bb42d54d115d734838e78d0
Fabio Galimberti <fabio.glimb@gmail.com>
2018-12-23 17:35:14 +0100

    except •

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,2 +1,2 @@
-export const removeNonNumber = (string = "") => string.replace(/[^\d]/g, "");
+export const removeNonNumber = (string = "") => string.replace(/[^\d|\•]/g, "");
 export const removeLeadingSpaces = (string = "") => string.replace(/^\s+/g, "");
